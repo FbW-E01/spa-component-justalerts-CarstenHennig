97f512ce7aaf686e9b04ebe7f7a2ab3dd6901e68
CarstenHennig <carsten.hennig@gmail.com>
2021-07-08 14:37:45 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
-
-  // ?????
-  
+const Box = (props) => {
+  return (
+    <div>
+      <h1
+        className={`alert alert-${
+          props.type === "blue"
+            ? "primary"
+            : props.type === "red"
+            ? "danger"
+            : props.type === "orange"
+            ? "warning"
+            : null
+        } role="alert" ${props.message === "Hidden box" ? "d-none" : null}`}
+      >
+        {props.message}
+      </h1>
+    </div>
+  );
 };
 
 // Do not edit below this line
-const jsx = <>
-  <Box type="blue" message="Blue box" />
-  <Box type="red" message="Red box" />
-  <Box type="orange" message="Orange box" />
-  <Box hide type="red" message="Hidden box" />
-</>;
+const jsx = (
+  <>
+    <Box type="blue" message="Blue box" />
+    <Box type="red" message="Red box" />
+    <Box type="orange" message="Orange box" />
+    <Box hide type="red" message="Hidden box" />
+  </>
+);
 const element = document.getElementById("target");
 
 ReactDOM.render(jsx, element);
